00ef3facc7829e073805692faa1f55bbf2545cfc
xingyue <qixingyue@126.com>
2021-12-02 01:17:33 +0000

    init task

--- debug.sh
+++ debug.sh
@@ -6,6 +6,6 @@ killall coredns
 apid=$!
 sleep 1 
 nslookup -port=5300 info.example.org 127.0.0.1
-nslookup -port=5300 info.example.orgc 127.0.0.1
+# nslookup -port=5300 info.example.orgc 127.0.0.1
 sleep 2
 kill $apid
